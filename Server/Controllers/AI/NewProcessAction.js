0838df88d148958018c2616211756a87f600f32e
Fistoflex <fabien.cheung-lim-yen@epitech.eu>
2022-10-16 16:43:34 +0400

    chore(serverAPI/MMACTION): cleaned folder, keeping only the useful files

--- Server/Controllers/AI/NewProcessAction.js
+++ Server/Controllers/AI/NewProcessAction.js
@@ -12,16 +12,21 @@ exports.newProcessAction = function (req, res) {
     const returnMessage = "You successfully Generate The Video";
     
     try {
-      let s3FilePathVideo = `${req.body.projectId}.mp4`;
-      videoObj = downloadFile(process.env.S3_BUCKET_RAW_VIDEO_AWS, s3FilePathVideo, true);
+       let s3FilePathVideo = `${req.body.projectId}.mp4`;
+      // videoObj = downloadFile(process.env.S3_BUCKET_RAW_VIDEO_AWS, s3FilePathVideo, true);
       
-      if (videoObj.Code === 84) {
-        throw Error.ERROR_S3_DOWNLOAD;
-      }
-      const pathToVideo = `./Files/S3Videos/${s3FilePathVideo}.mp4`;
-      const pathToResultJson = `./Files/ADVideosInfos/${req.body.projectId}.json`;
+      // if (videoObj.Code === 84) {
+      //   throw Error.ERROR_S3_DOWNLOAD;
+      // }
+      //const pathToVideo = `./Files/S3Videos/${s3FilePathVideo}.mp4`;
+      const pathToVideo = `./Files/S3Videos/${s3FilePathVideo}`;
+      const pathToResultJson = `./Files/ADVideoInfos/${req.body.projectId}.json`;
 
-      var child = exec(`python ../IA_MMACTION/demo/long_video_demo.py ../IA_MMACTION/configs/recognition/tsn/tsn_r50_video_inference_1x1x3_100e_kinetics400_rgb.py ../IA_MMACTION/checkpoints/tsn_r50_1x1x3_100e_kinetics400_rgb_20200614-e508be42.pth ${pathToVideo} ../IA_MMACTION/tools/data/kinetics/label_map_k400.txt ${pathToResultJson} --input-step 3 --device cpu --threshold 0.2`, (error, stdout, stderr) => {
+      console.log(`Path to video : ${pathToVideo}`);
+      console.log(`Path to result Json : ${pathToResultJson}`);
+      
+
+      var child = exec(`python ../AI_MMACTION/demo/long_video_demo.py ../AI_MMACTION/config/dataset-settings-modified.py ../AI_MMACTION/checkpoints/tsn_r50_1x1x3_100e_kinetics400_rgb_20200614-e508be42.pth ${pathToVideo} ../AI_MMACTION/tools/data/label_map_k400.txt ${pathToResultJson} --input-step 3 --device cpu --threshold 0.2`, (error, stdout, stderr) => {
         if (error) {
           console.log(`error: ${error.message}`);
           return;
